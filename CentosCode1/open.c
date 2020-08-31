90d33f8a5ee96a04df2c90be37f9ff11eb3b164d
SupersLWF <jianghuaiyue@outlook.com>
2020-08-31 11:39:10 +0800

    null

--- CentosCode1/open.c
+++ CentosCode1/open.c
@@ -6,11 +6,27 @@
 //int open(const char *name, int flags);
 //int open(const char *name, int flags, mode_t mode);
 
+//both of these two choices to open file are correct.
+
+//when you need to open a new file please use the second one,
+//and type the flag "O_CREAT",just like the second eg.    
+
 int main(){
 	int fd;
 
+	//1rd eg
 	fd = open("./Code1/test.txt",O_RDONLY);
 	if (fd==-1)
 		printf("file open error!");
+
+
+	//2nd eg
+	fd = open("./Code1/test.txt",				//CONST CHAR*
+		  O_CREAT|O_WRONLY|O_TRUNC,			//FLAGS
+		  S_IWUSR|S_IRUSR|S_IWGRP|S_IRGRP|S_IROTH);	//MODES
+
+	if (fd==-1)
+		printf("file open error!");
+
 	return 0;
 }
